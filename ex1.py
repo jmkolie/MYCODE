3ecc5aec710085879eb209ea42231cd9a04a7f08
jmkolie <kingchelmy@gmail.com>
2022-02-18 19:01:57 +0000

    bbonjour ceci est mon troisieme commit

---
+++ ex1.py
@@ -0,0 +1,8 @@
+a=int(input("donner le nombbre a:"))
+b=int(input("donnez le nombre b:"))
+if a>b:
+    print("le Max est: a")
+elif a==b:
+    print("les deux valeurs sont egaux")
+else:
+    print("le Max est: b")
